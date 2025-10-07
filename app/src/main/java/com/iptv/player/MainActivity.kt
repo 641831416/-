24779c11a53b6594a47b048e43276ad6ac35aa16
641831416 <168541988+641831416@users.noreply.github.com>
2025-10-07 13:10:09 +0800

    更新 MainActivity.kt

--- app/src/main/java/com/iptv/player/MainActivity.kt
+++ app/src/main/java/com/iptv/player/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.iptv.player
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
